eb8e03c7904e036b7c18bedb7c463a004e1d4eae
eungwang <scm323@naver.com>
2022-10-16 15:50:05 +0900

    Feat: auth-guard generate

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,52 @@
+import { UserService } from './../users/user.service';
+import { JwtService } from 'src/jwt/jwt.service';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { AllowedRoles } from './role.decorators';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  /** 비로그인 유저 - role이 지정되어 있지 않을 경우 통과
+   *  로그인 유저 - ANY일경우 ADMIN, CLIENT 모두 통과
+   *  로그인 유저 - 데코레이터에 명시한 Role과 자신의 Role이 일치 할 경우 통과
+   */
+  async canActivate(context: ExecutionContext) {
+    // Metadata에 부여한 role
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+    // 비로그인 유저 통과
+    if (!roles) {
+      return true;
+    }
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const token = gqlContext.req.headers['jwt-token'];
+    if (token) {
+      const decodedToken = this.jwtService.verify(token);
+      if (
+        typeof decodedToken === 'object' &&
+        decodedToken.hasOwnProperty('id')
+      ) {
+        const userId = decodedToken.id;
+        const { user } = await this.userService.userProfile({ id: userId });
+        if (user) {
+          gqlContext['user'] = user;
+        }
+        if (roles.includes('ANY')) {
+          return true;
+        }
+        return roles.includes(user.role);
+      }
+    }
+    return false;
+  }
+}

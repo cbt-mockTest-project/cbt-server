3461b991ac7cdd13c1a318070a4524c985d0fbac
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-10-19 12:23:52 +0900

    Feat: mockExamQuestionFeedBack dto generate

---
+++ src/mock-exams/dtos/createMockExamQuestionFeedback.dto.ts
@@ -0,0 +1,15 @@
+import { CoreOutput } from './../../common/dtos/output.dto';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { MockExamQuestionFeedback } from '../entities/mock-exam-question-feedback.entity';
+
+@InputType()
+export class CreateMockExamQuestionFeedbackInput extends PickType(
+  MockExamQuestionFeedback,
+  ['content'],
+) {
+  @Field(() => Number)
+  questionId: number;
+}
+
+@ObjectType()
+export class CreateMockExamQuestionFeedbackOutput extends CoreOutput {}

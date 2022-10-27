b387bef5f89e1f5f19bbc15ab1c835c3ef338e72
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-10-27 14:19:38 +0900

    Feat: enable cors setting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(8070);
   console.log('go to graphql : http://localhost:8070/graphql');
 }

34f667b750d87ba980c1f7ac24a6abb86d4fdbaa
eungwang <scm323@naver.com>
2022-12-23 02:08:06 +0900

    fix: cors

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,11 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
     await app.listen(80);
     console.log('go to graphql : http://localhost:80/graphql');
 }

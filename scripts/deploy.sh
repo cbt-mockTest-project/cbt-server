4057d9f4c18243d7089dad462d2f9b483f1cfccf
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-12-26 17:50:12 +0900

    Feat: modify script of deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+sed -i '.bak' "/\/dist/d" .gitignore
 git reset --hard origin/release
 yarn
 yarn build
-rm -rf node_modules
 rm -rf src
 rm -rf .gitignore
 git add .
 git commit -m "deploy"
 git push origin deploy --force
+

7237300bb75a0040494033887e4aa3da7172829d
eungwang <scm323@naver.com>
2023-04-16 17:35:17 +0900

    Fix: exception error 이슈 해결

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -12,7 +12,6 @@ import logger from 'src/lib/logger';
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: unknown, host: ExecutionContext) {
-    const ctx = host.switchToHttp();
     const isGraphQL = host.getType<GqlContextType>() === 'graphql';
 
     let status: number;
@@ -24,20 +23,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
     if (isGraphQL) {
       const gqlCtx = GqlExecutionContext.create(host);
       const request = gqlCtx.getContext().req;
-      const response = gqlCtx.getContext().res;
+
       logger.error(`Request: ${request.method} ${request.originalUrl}`);
       logger.error(`Body: ${JSON.stringify(request.body, null, 2)}`);
       logger.error(
         `Response: ${status} Error: ${JSON.stringify(exception, null, 2)}`,
       );
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
+      throw exception; // 에러를 직접 던지도록 변경
     } else {
+      const ctx = host.switchToHttp();
       const req = ctx.getRequest<Request>();
       const res = ctx.getResponse<Response>();
+
       logger.error(`Request: ${req.method} ${req.originalUrl}`);
       logger.error(`Body: ${JSON.stringify(req.body, null, 2)}`);
       logger.error(

b2308ac3dbb60b2a943c2506b6aeeb3902460640
eungwang <scm323@naver.com>
2023-06-13 01:39:16 +0900

    Feat: pm2 셋팅 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,12 @@ module.exports = {
     {
       name: 'moducbt-server',
       script: 'dist/main.js',
-      instances: 0,
+      instances: 'max',
       exec_mode: 'cluster', // cluster mode
       wait_ready: true,
+      autorestart: true, // 프로세스 실패시 자동 재시작
+      watch: false, // 파일 변경 감시 비활성화
+      max_memory_restart: '1G', // 메모리 최대 사용량 초과시 재시작
       instance_var: 'moducbt',
       listen_timeout: 50000,
       kill_timeout: 5000,

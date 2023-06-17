015665de8698d8a96d4e6d43b1c0a151d58bc895
eungwang <scm323@naver.com>
2023-06-17 22:50:59 +0900

    Feat: todo entity 작성

---
+++ src/todo/entities/Todo.entity.ts
@@ -0,0 +1,28 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+
+@InputType('TodoListInputType', { isAbstract: true })
+@ObjectType()
+export class TodoList {
+  @Field(() => String)
+  todo: string;
+}
+
+@InputType('TodoInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Todo extends CoreEntity {
+  @Column({ type: 'json', default: [] })
+  @Field(() => [TodoList])
+  todoList: TodoList[];
+
+  @Column()
+  @Field(() => String)
+  dateString: string;
+
+  @ManyToOne(() => User, (user) => user.todos)
+  @Field(() => User)
+  user: User;
+}

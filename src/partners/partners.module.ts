1e6254634dccf082ae42c5e9d93e5167d3cdf1c2
eungwang1 <scm323@naver.com>
2023-07-04 14:26:34 +0900

    Feat: getPartenrs api 작성

--- src/partners/partners.module.ts
+++ src/partners/partners.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Partner } from './entities/partners.entity';
+import { PartnersResolver } from './partners.resolver';
+import { PartnersService } from './partners.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Partner])],
+  providers: [PartnersResolver, PartnersService],
+})
 export class PartnersModule {}

26e0333498a7b6fe9d3aa42f7397007783f22731
eungwang1 <scm323@naver.com>
2023-12-15 11:09:14 +0900

    Feat: bookmark 복합키 설정

--- src/mock-exam-bookmark/entities/mock-exam-bookmark.entity.ts
+++ src/mock-exam-bookmark/entities/mock-exam-bookmark.entity.ts
@@ -1,16 +1,25 @@
-import { InputType, ObjectType } from '@nestjs/graphql';
-import { CoreEntity } from 'src/common/entities/core.entity';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { MockExam } from 'src/mock-exams/entities/mock-exam.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
 @InputType('MockExamBookmarkInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
-export class MockExamBookmark extends CoreEntity {
+export class MockExamBookmark {
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  examId: number;
+
   @ManyToOne(() => User, (user) => user.examBookmarks)
+  @JoinColumn({ name: 'userId' })
+  @Field(() => User)
   user: User;
 
   @ManyToOne(() => MockExam, (exam) => exam.examBookmarks)
+  @JoinColumn({ name: 'examId' })
+  @Field(() => MockExam)
   exam: MockExam;
 }

1d7c5cebe4571af5fc662ccd2918b8ee12dc0528
eungwang1 <scm323@naver.com>
2024-05-13 09:12:23 +0900

    chore: 스케줄 시간 수정

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -67,44 +67,6 @@ export class SchedulerService {
   //   }
   // }
 
-  // 오전 4시
-  @Cron('0 0 4 * * *', { timeZone: 'Asia/Seoul' })
-  async revalidateQuestion() {
-    // try {
-    //   if (process.env.NODE_ENV === 'dev') {
-    //     return;
-    //   }
-    //   async function* generateBatches(questionIds, batchSize) {
-    //     for (let i = 0; i < questionIds.length; i += batchSize) {
-    //       yield questionIds.slice(i, i + batchSize);
-    //     }
-    //   }
-    //   const res = await this.mockExamQuestions.find();
-    //   const questionIds = res.map((el) => el.id);
-    //   const batchSize = 30; // 한 번에 처리할 질문의 수
-    //   for await (const batch of generateBatches(questionIds, batchSize)) {
-    //     await Promise.all(
-    //       batch.map((id) =>
-    //         this.revalidateService.revalidate({
-    //           path: `/question/${id}`,
-    //         }),
-    //       ),
-    //     );
-    //     await new Promise((resolve) => setTimeout(resolve, 5000));
-    //   }
-    //   this.telegramService.sendMessageToTelegram({
-    //     message: `cronjob: revalidate success`,
-    //     channelId: Number(process.env.TELEGRAM_ALRAM_CHANNEL),
-    //   });
-    // } catch {
-    //   this.telegramService.sendMessageToTelegram({
-    //     message: `cronjob: revalidate error`,
-    //     channelId: Number(process.env.TELEGRAM_ALRAM_CHANNEL),
-    //   });
-    //   console.log('cronjob: revalidate error');
-    // }
-  }
-
   //3분에 한번
   // @Interval(1000 * 60 * 1)
   // async pong() {
@@ -122,9 +84,8 @@ export class SchedulerService {
   //   }
   // }
 
-  // 1시간에 1번
-  // @Cron('0 0 4 * * *', { timeZone: 'Asia/Seoul' })
-  @Interval(3 * 1000)
+  // 새벽 4시
+  @Cron('0 0 4 * * *', { timeZone: 'Asia/Seoul' })
   async clearBasicPlan() {
     try {
       if (process.env.NODE_ENV === 'dev') {

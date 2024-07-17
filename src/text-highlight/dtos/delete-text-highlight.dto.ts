fc249159b46b42e1a5ddb61b65a364e13db6947e
eungwang1 <scm323@naver.com>
2024-07-17 13:05:00 +0900

    feat: highlight-text dto

--- src/text-highlight/dtos/delete-text-highlight.dto.ts
+++ src/text-highlight/dtos/delete-text-highlight.dto.ts
@@ -1,8 +1,11 @@
-import { InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 
 @InputType()
-export class DeleteTextHighlightInput {}
+export class DeleteTextHighlightInput {
+  @Field(() => String)
+  textHighlightId: string;
+}
 
 @ObjectType()
 export class DeleteTextHighlightOutput extends CoreOutput {}

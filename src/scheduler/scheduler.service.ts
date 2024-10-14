c880f7322aaabf7f0bb19a3d5746f3c8db730607
eungwang <scm323@naver.com>
2024-10-15 08:01:46 +0900

    스케줄러 수정

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,53 +1,43 @@
 import { TelegramService } from './../telegram/telegram.service';
-import { MockExamQuestion } from '../exam/entities/mock-exam-question.entity';
-import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
-import { VisitService } from 'src/visit/visit.service';
 import { UserService } from 'src/users/user.service';
 import { MockExamQuestionFeedbackSerivce } from 'src/exam/mock-exams-question-feedback.service';
 import { MockExam } from 'src/exam/entities/mock-exam.entity';
-import { CoupangService } from 'src/modu-shop/coupang/coupang.service';
 import { MockExamCategory } from 'src/exam-category/entities/mock-exam-category.entity';
 
 @Injectable()
-export class SchedulerService {
+export class SchedulerService implements OnModuleInit {
+  private isMaster: boolean;
   constructor(
-    @InjectRepository(MockExamQuestion)
-    private readonly mockExamQuestions: Repository<MockExamQuestion>,
     @InjectRepository(MockExam)
     private readonly mockExams: Repository<MockExam>,
     @InjectRepository(MockExamCategory)
     private readonly mockExamCategories: Repository<MockExamCategory>,
-    private readonly visitService: VisitService,
     private readonly telegramService: TelegramService,
     private readonly userService: UserService,
     private readonly mockExamQuestionFeedbackService: MockExamQuestionFeedbackSerivce,
-    private readonly coupangService: CoupangService,
+    private readonly schedulerRegistry: SchedulerRegistry,
   ) {}
 
-  // // 매일 밤 12시
-  @Cron('0 59 23 * * *', { timeZone: 'Asia/Seoul' })
-  async clearVisit() {
-    if (process.env.NODE_ENV === 'dev') {
-      return;
-    }
-    try {
-      await this.visitService.clearVisit();
-    } catch {
-      await this.telegramService.sendMessageToTelegram({
-        message: `방문자수 초기화 실패`,
-        channelId: Number(process.env.TELEGRAM_ALRAM_CHANNEL),
+  onModuleInit() {
+    this.isMaster = process.env.NODE_APP_INSTANCE === '0';
+    const isDev = process.env.NODE_ENV === 'dev';
+    const isCronJobInstance = process.env.CRON_JOB === 'ok';
+    if (!this.isMaster || isDev || isCronJobInstance) {
+      const cronJobs = this.schedulerRegistry.getCronJobs();
+      cronJobs.forEach((job, jobName) => {
+        this.schedulerRegistry.deleteCronJob(jobName);
       });
     }
   }
-
   // 새벽 3시
   @Cron('0 0 3 * * *', { timeZone: 'Asia/Seoul' })
   async hideNegativeFeedbacks() {
     try {
-      if (process.env.NODE_ENV === 'dev') {
+      if (process.env.NODE_ENV === 'dev' && process.env.CRON_JOB === 'ok') {
         return;
       }
       // recommendation.type === 'BAD' 인 요소를 3개 이상 가진 feedback private로 변경
@@ -67,7 +57,7 @@ export class SchedulerService {
   @Cron('0 0 3 * * *', { timeZone: 'Asia/Seoul' })
   async manageExamApproval() {
     try {
-      if (process.env.NODE_ENV === 'dev') {
+      if (process.env.NODE_ENV === 'dev' && process.env.CRON_JOB === 'ok') {
         return;
       }
       const exams = await this.mockExams
@@ -110,7 +100,7 @@ export class SchedulerService {
   @Cron('0 0 7 * * *', { timeZone: 'Asia/Seoul' })
   async clearBasicPlan() {
     try {
-      if (process.env.NODE_ENV === 'dev') {
+      if (process.env.NODE_ENV === 'dev' && process.env.CRON_JOB === 'ok') {
         return;
       }
       this.userService.clearBasicRole();
@@ -129,6 +119,9 @@ export class SchedulerService {
   @Cron('0 0 2 * * *', { timeZone: 'Asia/Seoul' })
   async syncEvaluationCount() {
     try {
+      if (process.env.NODE_ENV === 'dev' && process.env.CRON_JOB === 'ok') {
+        return;
+      }
       const mockExamCategories = await this.mockExamCategories.find({
         relations: {
           categoryEvaluations: true,
@@ -145,31 +138,6 @@ export class SchedulerService {
     } catch {
       this.telegramService.sendMessageToTelegram({
         message: `cronjob: syncEvaluationCount error`,
-        channelId: Number(process.env.TELEGRAM_ALRAM_CHANNEL),
-      });
-    }
-  }
-  // 새벽 6시
-  @Cron('0 0 6 * * *', { timeZone: 'Asia/Seoul' })
-  async updateProductList() {
-    try {
-      const keywords = [
-        '독서대',
-        '스터디 플래너',
-        '공부타이머',
-        '지워지는볼펜',
-        '전자노트',
-        '에너지드링크',
-        '공책',
-        '노트북 거치대',
-        '포스트잇',
-      ];
-      keywords.forEach((keyword) => {
-        this.coupangService.crawlProductListFromCoupang(keyword);
-      });
-    } catch {
-      this.telegramService.sendMessageToTelegram({
-        message: `cronjob: updateProductList error`,
         channelId: Number(process.env.TELEGRAM_ALRAM_CHANNEL),
       });
     }
